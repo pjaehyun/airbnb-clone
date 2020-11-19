882da7b9efbd18b7039f3ba4b6744b6314d0847a
JaehyunPark-S <show6756@gmail.com>
2020-11-20 02:35:21 +0900

    seed data

---
+++ rooms/management/commands/seed_amenities.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from rooms.models import Amenity
+
+
+class Command(BaseCommand):
+
+    help = "This command creates amenities"
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument(
+    #         "--times",
+    #         help="How many thimes do you want me to tell you that I love you?",
+    #     )
+
+    def handle(self, *args, **options):
+        amenities = [
+            "Kitchen",
+            "Shampoo",
+            "Heating",
+            "Air conditioning",
+            "Washer",
+            "Dryer",
+            "Wifi",
+            "Breakfast",
+            "Indoor fireplace",
+            "Hangers",
+            "Iron",
+            "Hair dryer",
+            "Laptop-friendly workspace",
+            "TV",
+            "Crib",
+            "High chair",
+            "Self check-in",
+            "Smoke alarm",
+            "Carbon monoxide alarm",
+            "Private bathroom",
+            "Piano",
+            "Beachfront",
+            "Waterfront",
+        ]
+        for a in amenities:
+            Amenity.objects.create(name=a)
+
+        self.stdout.write(self.style.SUCCESS("Amenities created"))
---
+++ users/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+from rooms import models as room_models
+
+
+class RoomInline(admin.TabularInline):
+    model = room_models.Room
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    inlines = (RoomInline,)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )

984077285006e00ccc9784c9a8f78e09833ac754
JaehyunPark-S <show6756@gmail.com>
2020-11-21 19:53:17 +0900

    FBV search

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "9-0q-u51vxw)_uy1u#u98!qf=(yy+1m=x(f$g4htoxy#d7l@uu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition

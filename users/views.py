390370c35dd7f921edac4d7da874ba3107ee9534
JaehyunPark-S <show6756@gmail.com>
2021-01-18 23:01:08 +0900

    Intercept email

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from django.views import View
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
@@ -209,7 +209,34 @@ class UserProfileView(DetailView):
     model = models.User
     context_object_name = "user_obj"
 
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "email",
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        email = form.clean_data.get("email")
+        self.object.username = email
+        self.object.save()
+        return super().form_valid(form)
+
     """ FBV Login """
+
     # def get(self, request):
     #     form = forms.LoginForm()
     #     return render(request, "users/login.html", {"form": form})
